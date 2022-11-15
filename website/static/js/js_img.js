84d422046f850f9ffc4bcfb704e0f77246cb30f4
valentin.bremont@gmail.com"Valentin <valentin.bremont@gmail.com>
2022-11-15 19:13:59 +0100

    change pieces

---
+++ website/static/js/js_img.js
@@ -0,0 +1,20 @@
+//<div class="divv" id="row2">
+//<li class="case case_white" id="case_A2"><img class="piece" src="{{ url_for('static', filename='img_piece/maestro/wP.svg') }}"></li>
+//<li class="case case_black" id="case_B2"><img class="piece" src="{{ url_for('static', filename='img_piece/maestro/wP.svg') }}"></li>
+//<li class="case case_white" id="case_C2"><img class="piece" src="{{ url_for('static', filename='img_piece/maestro/wP.svg') }}"></li>
+//<li class="case case_black" id="case_D2"><img class="piece" src="{{ url_for('static', filename='img_piece/maestro/wP.svg') }}"></li>
+//<li class="case case_white" id="case_E2"><img class="piece" src="{{ url_for('static', filename='img_piece/maestro/wP.svg') }}"></li>
+//<li class="case case_black" id="case_F2"><img class="piece" src="{{ url_for('static', filename='img_piece/maestro/wP.svg') }}"></li>
+//<li class="case case_white" id="case_G2"><img class="piece" src="{{ url_for('static', filename='img_piece/maestro/wP.svg') }}"></li>
+//<li class="case case_black" id="case_H2"><img class="piece" src="{{ url_for('static', filename='img_piece/maestro/wP.svg') }}"></li>
+//</div>
+//<div class="divv" id="row1">
+//<li class="case case_black" id="case_A1"><img class="piece" src="{{ url_for('static', filename='img_piece/maestro/wR.svg') }}"></li>
+//<li class="case case_white" id="case_B1"><img class="piece" src="{{ url_for('static', filename='img_piece/maestro/wN.svg') }}"></li>
+//<li class="case case_black" id="case_C1"><img class="piece" src="{{ url_for('static', filename='img_piece/maestro/wB.svg') }}"></li>
+//<li class="case case_white" id="case_D1"><img class="piece" src="{{ url_for('static', filename='img_piece/maestro/wQ.svg') }}"></li>
+//<li class="case case_black" id="case_E1"><img class="piece" src="{{ url_for('static', filename='img_piece/maestro/wK.svg') }}"></li>
+//<li class="case case_white" id="case_F1"><img class="piece" src="{{ url_for('static', filename='img_piece/maestro/wB.svg') }}"></li>
+//<li class="case case_black" id="case_G1"><img class="piece" src="{{ url_for('static', filename='img_piece/maestro/wN.svg') }}"></li>
+//<li class="case case_white" id="case_H1"><img class="piece" src="{{ url_for('static', filename='img_piece/maestro/wR.svg') }}"></li>
+//</div>
