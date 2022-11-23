3f9565764da93f2a169d6bcaf4b21fad258fddcb
PlacidFox <valentin.bremont@outlook.fr>
2022-11-23 13:19:36 +0100

    OK fetch JSON data

--- website/static/js/training_mode.js
+++ website/static/js/training_mode.js
@@ -1,16 +1,34 @@
 const color = "red";
 
 let value;
+let color_pgn;
+let pgn_name;
+let moves_uci;
 
-function fetch_value(value_test){
-    value = value_test;
+let json_data;
+
+function fetch_json(data_json_f){
+    json_data = data_json_f;
 
 }
 
-function training_test(){
-    document.getElementById("textp").innerHTML = "Test Training Mode - White : " + value;
 
-    //setBoard("white");
+function training_test(){
+
+    document.getElementById("textp").innerHTML = "Test Training Mode " + json_data["color"] + " : " + json_data["games"][0]["name"];
+    document.getElementById("moves-header-TEMPO").innerHTML = json_data["games"][0]["name"];
+
+    json_data["games"][0]["moves_uci"].forEach(element => {
+        console.log(element["from_square"])
+
+        if (element["color"] == "White") { color_to_play = 1}
+        if (element["color"] == "Black") { color_to_play = 2}
+
+        set_list_moves(element["from_square"].toUpperCase(),element["to_square"].toUpperCase(), color_to_play)
+    });
+
+    //document.getElementById("table-moves").innerHTML = 
+
 
 }
 
--- website/views.py
+++ website/views.py
@@ -1,14 +1,35 @@
 from flask import Blueprint, render_template
 
+import os, json
+
+JSON_PATH = "./data/test_json.json"
+
 views = Blueprint("views", __name__)
 
 @views.route("/")
 def home():
     
-    value_test = "caro-kann"
-    
-    return render_template("base.html", value_test = value_test)
+    return render_template("base.html")
 
-#remettre base si bug ?
+@views.route("/training-white")
+def trainingwhite():
+    value_test = "Training White : Caro-kann"
+    
+    if os.path.exists(JSON_PATH):
+        with open(JSON_PATH, "r") as jsonFile:
+            data_json = json.load(jsonFile)        
+
+
+    color = data_json["color"]
+    pgn_name = data_json["games"][0]["name"]
+    moves_uci_data = data_json["games"][0]["moves_uci"]  
+    
+    moves_uci = []
+    
+    for move in moves_uci_data:
+        moves_text = move["from_square"] + move["to_square"]
+        moves_uci.append(moves_text)
+    
+    return render_template("trainingwhite.html", value_test = value_test, color = color, pgn_name = pgn_name, moves_uci = moves_uci, data_json = data_json)
 
 
