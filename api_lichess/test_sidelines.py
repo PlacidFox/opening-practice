ea3d16966d7e13b1f0532eeb4b7411584afb71c8
Valentin <valentin.bremont@gmail.com>
2022-12-05 22:13:32 +0100

    test fetch sideline

--- api_lichess/test_sidelines.py
+++ api_lichess/test_sidelines.py
@@ -8,94 +8,86 @@ def test_split_game_moves(game):
 
 games = open_pgn(NAME_PGN[Colors.TEST_WHITE])
 
+
+def getsidelinesNumber(game):
+    nb_variation = 1
+    end_move = False
+       
+    while not end_move:
+        game = game.next()
+        
+        if len(game.variations) > 1:
+            new_variation = len(game.variations) - 1
+            nb_variation += new_variation
+
+            for nb_var in range (new_variation) :                
+                game_tempo = game.variations[nb_var + 1]                        
+                variationcheckloop(game_tempo)
+
+        end_move = game.is_end()   
+
+    print (nb_variation)
+        
+
+def variationcheckloop(game_tempo_loop):
+    end_move = False
+    
+    while not end_move:
+        game_tempo_loop = game_tempo_loop.next()
+        
+        if len(game_tempo_loop.variations) > 1:
+            new_variation = len(game_tempo_loop.variations) - 1
+            nb_variation += new_variation
+            
+            for nb_var in range (new_variation) :
+                
+                new_game_tempo_loop = game_tempo_loop.variations[nb_var + 1]
+                        
+                variationcheckloop(new_game_tempo_loop)
+                        
+        end_move = game_tempo_loop.is_end()   
+    
+   
+def getsidelines(game):# work for only 1 sideline and only one time, no sideline later on
+    variations_moves = [[]]
+    end_move = False
+       
+    while not end_move:
+        game = game.next()
+        
+        variations_moves[0].append(str(game).split(" ")[1])
+                
+        if len(game.variations) > 1:
+
+            for index in range(1, len(game.variations)):
+                end_move = False
+                
+                game_variation = game.variations[index]
+                
+                variations_moves.append([])            
+                for move in variations_moves[0]:
+                    variations_moves[index].append(move)
+                            
+                variations_moves[index].append(str(game_variation).split(" ")[1])
+                            
+                while not end_move:
+                    game_variation = game_variation.next()        
+                    variations_moves[index].append(str(game_variation).split(" ")[1])
+                    print(str(game_variation).split(" ")[1])
+                    end_move = game_variation.is_end()
+            
+        end_move = game.is_end()
+
+    print (variations_moves)
+
+   
+
+
+getsidelines(games[3])
+
 """
 for game in games:
-
-    print(test_split_game_moves(game))
-    print(game.mainline())
-    print(game.variations)        
-    #game.next(game.variations[0])
-    game.variations[0].next()
-    print(game.variations)  
-    
-    print(game.variations[0].next().next().next().next().variations)
-    
-    print(game.variations[0].next().next().next().next().variations[1])
-    
-    print(game.variations[0].next().next().next().next())    
-""" 
-
-
-def getsidelinesNumber(game):
-    nb_variation = 1
-    end_move = False
-       
-    while not end_move:
-        game = game.next()
-        
-        print(str(game).split(" ")[1])
-                
-        if len(game.variations) > 1:
-            nb_variation += 1
-            
-            game_2 = game.variations[1]
-                        
-            while not end_move:
-                game_2 = game_2.next()        
-                print(str(game_2).split(" ")[1])
-                end_move = game_2.is_end()
-                       
-        end_move = game.is_end()
-
-    print (nb_variation)
-    
-   
-def getsidelinesNumber(game):
-    nb_variation = 1
-    end_move = False
-       
-    while not end_move:
-        game = game.next()
-        
-        print(str(game).split(" ")[1])
-                
-        if len(game.variations) > 1:
-            nb_variation += 1
-            
-            game_2 = game.variations[1]
-                        
-            while not end_move:
-                game_2 = game_2.next()        
-                print(str(game_2).split(" ")[1])
-                end_move = game_2.is_end()
-                       
-        end_move = game.is_end()
-
-    print (nb_variation)
-
-    
-
-    
-    
-def split_game(game):
-    tab_cible = []
-
-    for i in range (10):
-        game = game.next()
-        if game is None:
-            break
-        
-        move = str(game).split(" ")[1]
-        tab_cible.append(move)
-        
-        print(game)
-   
-        if len(game.variations) > 1:
-            game = game.variations[1]
-            move = str(game).split(" ")[1]
-            tab_cible.append(move)
-            print(game)
-
-for game in games:
     #split_game(game)
-    getsidelinesNumber(game)
+    #getsidelinesNumber(game)
+    getsidelines(game)
+"""
